3a26565084e4d5cdd52ac1fd098b74e18db76656
Jovan Ristic <jristic42@gmail.com>
2019-09-04 22:22:34 -0700

    Detours files

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ call project.bat
 
 set BuildFolder=build
 
-set DetoursIncludePath="Detours\src"
-set DetoursLibPath="Detours\lib"
+set DetoursIncludePath="external\Detours\include"
+set DetoursLibPath="external\Detours\lib.X64"
 set CommonCompilerFlags=/MTd /nologo /fp:fast /Gm- /GR- /EHa- /Od /Oi /WX /W4 /wd4201 /FC /Z7 /D_CRT_SECURE_NO_WARNINGS /I%DetoursIncludePath% /Fo%BuildFolder%\
-set CommonLinkerFlags=/incremental:no /opt:ref /subsystem:console /libpath:%VulkanLibPath% /libpath:%DetoursLibPath% detours.lib
+set CommonLinkerFlags=/incremental:no /opt:ref /subsystem:console /libpath:%DetoursLibPath% detours.lib
 
 set AdditionalSourceFiles=
 
